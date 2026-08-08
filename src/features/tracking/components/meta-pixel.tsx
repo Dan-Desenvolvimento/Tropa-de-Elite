eddc40dc2d881d29a682ffa4b7fc7fef135b729b
James Nebou <jamesnebou34@gmail.com>
2026-08-07 22:45:37 -0300

    Pixel

--- src/features/tracking/components/meta-pixel.tsx
+++ src/features/tracking/components/meta-pixel.tsx
@@ -27,7 +27,7 @@ export function MetaPixel() {
   }, []);
   return (
     <>
-      <Script id="meta-pixel" strategy="afterInteractive">
+      <Script id="meta-pixel" strategy="beforeInteractive">
         {`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
 n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
