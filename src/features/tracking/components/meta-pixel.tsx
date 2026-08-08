a527e27ea5a53ebc0a4d7c3bce0076d769f07630
James Nebou <jamesnebou34@gmail.com>
2026-08-07 21:00:27 -0300

    atualiza pixel

--- src/features/tracking/components/meta-pixel.tsx
+++ src/features/tracking/components/meta-pixel.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import Script from "next/script";
 import { useEffect } from "react";
+
+const FALLBACK_PIXEL_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID || "1760053031291739";
 
 declare global {
   interface Window { fbq?: (...args: unknown[]) => void; }
@@ -12,7 +15,7 @@ export function MetaPixel() {
     fetch("/api/tracking/config", { cache: "no-store" })
       .then((response) => response.json() as Promise<{ pixelId: string | null }>)
       .then(({ pixelId }) => {
-        if (!active || !pixelId || document.querySelector("[data-meta-pixel]") ) return;
+        if (!active || !pixelId || window.fbq) return;
         const script = document.createElement("script");
         script.dataset.metaPixel = "true";
         script.async = true;
@@ -22,5 +25,25 @@ export function MetaPixel() {
       .catch(() => undefined);
     return () => { active = false; };
   }, []);
-  return null;
+  return (
+    <>
+      <Script id="meta-pixel" strategy="afterInteractive">
+        {`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,
+'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','${FALLBACK_PIXEL_ID}');
+fbq('track','PageView');`}
+      </Script>
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src={`https://www.facebook.com/tr?id=${FALLBACK_PIXEL_ID}&ev=PageView&noscript=1`}
+          alt=""
+        />
+      </noscript>
+    </>
+  );
 }
